e00d7042b2533d87ee074adceb11e51c4efeab98
jyKim-97 <rlawjddud73@khu.ac.kr>
2022-05-05 01:26:08 +0900

    code refactoring & add description of the code

--- kuramoto.py
+++ kuramoto.py
@@ -1,31 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import pickle as pkl
 from tqdm import tqdm
 import cv2
 
 
 class SpatialKuramoto:
-    def __init__(self, L, K, natural_freq_distrib="uniform", **natural_params):
-        self.L = L
+    """
+    Simulate Kuramoto oscillator
+    ============================
+    Input:
+
+    N: The number of the oscillators
+    K: coupling strength
+    natural_freq_distrib: probability distribution type of the natural frequency
+        - const: delta function distribution, need "w" as arguments which determines natural freq.
+        - uniform: uniform distribution, need "w_a", "w_b" as arguments which determine the range for prob.
+        - gaussian: gaussian distribution, need "w_mu", "w_sgm" as arguments which determine mean & standard deviation of prob.
+    
+    network: type of the network
+        - full: fully connected network (all-to-all)
+        - sptial: connect only nearest neighbor units
+
+    natural_params: arguemnts of natural_freq_distrib. see above.
+
+    ============================
+    Example
+    obj = ko.SpatialKuramoto(10000, 1, "gaussian", w_mu=3, w_sgm=1, network="spatial")
+    obj.run(tmax=10, dt=0.01)
+    obj.save(<file_name_to_save_phase_data_to_time>)
+    """
+
+    def __init__(self, N, K, natural_freq_distrib="uniform", network="spatial", **natural_params):
         self.K = K
-        self.num_osc = L*L
-        self._set_simulation(natural_freq_distrib, natural_params)
+        self.num_osc = N
+        self.L = int(np.sqrt(N))
+        self._set_simulation(natural_freq_distrib, network, natural_params)
         
-    def run(self, tmax=100, dt=0.01, save_as_file=False, save_name=None):
-        
+    def run(self, tmax=100, dt=0.01, save_name=None):
+        # tmax: maximum simulation time
+        # dt: time step of the simulation
         self.dt = dt
         self.tmax = tmax
+        self.ts = np.arange(0, tmax, dt)
         self.num_itr = int(tmax/dt)
-        self.save_as_file = save_as_file
-        self._init_writer(save_name)
-        self.phase = np.random.random(self.num_osc)*np.pi
+        self.phase = np.random.random(self.num_osc)*2*np.pi
+        self.phase_all = np.zeros([self.num_itr, self.num_osc])
         
         for n in tqdm(range(self.num_itr)):
             self.update()
-            self.write(n)
-            
-        self._close_writer()
+            self.phase_all[n] = self.phase
         
     def f(self, p):
         dp = np.zeros(self.num_osc)
@@ -42,29 +67,14 @@ class SpatialKuramoto:
         dp = (k1 + 2*k2 + 2*k3 + k4)/6
         self.phase = self.phase + dp
     
-    def write(self, nstep):
-        if self.save_as_file:
-            np.save(self.fp, self.phase)
-        else:
-            self.phase_all[nstep] = self.phase
-    
-    def _init_writer(self, save_name):
-        if self.save_as_file:
-            if save_name is None:
-                date = time.localtime()
-                save_name = "%02d%02d%02d"%(date.tm_hour, date.tm_min, date.tm_sec)
-            self.fp = fopen(save_name+".npy", "wb")
-            self.phase_all = None
-        else:
-            self.phase_all = np.zeros([self.num_itr, self.num_osc])
-        
-    def _close_writer(self):
-        if self.save_as_file:
-            self.fp.close()
-    
-    def _set_simulation(self, natural_freq_distrib, natural_params):
+    def _set_simulation(self, natural_freq_distrib, network_type, natural_params):
         self.phase = np.zeros(self.num_osc)
-        self._gen_network()
+        if network_type == "spatial":
+            self._gen_network_spatial()
+        elif network_type == "full":
+            self._gen_network_full()
+        else:
+            print("Selected wrong network type")
         
         if natural_freq_distrib == "uniform":
             self._set_natural_freq_uniform(natural_params)
@@ -75,7 +85,7 @@ class SpatialKuramoto:
         else:
             print("Selected wrong prob distribution")
         
-    def _gen_network(self):
+    def _gen_network_spatial(self):
         # periodic boundary condition
         self.ntk = []
         # DIR = (1, -1, self.L, -self.L)
@@ -96,6 +106,12 @@ class SpatialKuramoto:
                 n_post = i + d
                 self.ntk[-1].append(n_post)
     
+    def _gen_network_full(self):
+        self.ntk = []
+        for i in range(self.num_osc):
+            tmp = [j for j in range(self.num_osc) if j != i]
+            self.ntk.append(tmp)
+    
     def _set_natural_freq_gaussian(self, natural_params):
         self._check_key_in(natural_params, "w_mu", "w_sgm")
         self.w = np.random.random(self.num_osc) * natural_params["w_sgm"] + natural_params["w_mu"]
@@ -114,14 +130,36 @@ class SpatialKuramoto:
         for k in key_names:
             if k not in keys_dict:
                 print(f"{k} is not given as the parameters")
-
+        
+    def save(self, save_name):
+        print("print data to %s"%(save_name))
+        with open(save_name, "wb") as fid:
+            pkl.dump(fid, self.phase_all)
+        
+    def load(self, fname):
+        print("load data %s"%(fname))
+        with open(fname, "rb") as fid:
+            self.phase_all = pkl.load(fid)
+                
 
 class VideoMaker:
+    """
+    Make video of the simulation data 
+    ============================
+    Input:
+    objKuramoto: SpatialKuramoto object of "spatial" network type after execute "run" method
+    ============================
+    Example
+    video = VideoMaker(obj)
+    video.export(<video_name>, frame_itv=1, fps=30)
+    """
     def __init__(self, objKuramoto):
         self.obj = objKuramoto
         self.ims = np.sin(reshape(self.obj.phase_all))
     
     def export(self, out_name=None, dpi=120, figsize=(4,4), frame_itv=1, fps=30):
+        # frame_itv: simulation step interval of the video
+        # fps: video fps
         self.draw_init(dpi, figsize)
         self.init_video(out_name, fps)
         
@@ -151,9 +189,6 @@ class VideoMaker:
         plt.axis('off')
         plt.tight_layout()
         self.fig.canvas.draw()
-
-        # get frame size
-        # self.frame = get_frame(self.fig)
     
     def draw(self, n_frame):
         self.im_map.set_array(self.ims[n_frame])
@@ -163,31 +198,7 @@ class VideoMaker:
 def get_frame(fig):
     frame = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
     return frame.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-                
-                
-def draw_single_im(frame):
-    im_map.set_data(np.sin(ims[frame]))
-    return im_map,
-                
-def draw_animation(objKuramoto, out_name=None, dpi=120, figsize=(4,4), frame_itv=1, fps=30, cmap='RdBu'):
-    from matplotlib.animation import FuncAnimation, writers
-    
-    sin_phs = reshape(np.sin(objKuramoto.phase_all))
-    
-    fig = plt.figure(dpi=dpi, figsize=figsize)
-    im_map = plt.imshow(sin_phs[0], cmap=cmap, vmax=1, vmin=-1)
-    plt.axis('off')
-    
-    ani = FuncAnimation(fig, draw_single_im, frames=np.arange(0, ims.shape[0], 1), blit=True)
-    writer = writers['ffmpeg'](fps=60)
-    
-    if out_name is None:
-        date = time.localtime()
-        save_name = "%02d%02d%02d"%(date.tm_hour, date.tm_min, date.tm_sec)
-    print(f"save result as {out_name}.mp4")
-    ani.save(f"{out_name}.mp4", writer=writer, dpi=dpi)
-    print("Done")
-                
+
                 
 def reshape_single(phase):
     L = int(np.sqrt(phase.shape[0]))
@@ -198,6 +209,10 @@ def reshape(phase):
     N = phase.shape[0]
     L = int(np.sqrt(phase.shape[1]))
     return phase.reshape(N, L, L)
+
+
+def get_r(phase_all):
+    return np.abs(np.average(np.exp(1j*phase_all), axis=1))
 
 
 if __name__ == "__main__":
